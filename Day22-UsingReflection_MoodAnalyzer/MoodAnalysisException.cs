6d7118a801f3bb4008b6a1a64fca9f9c589f8ed8
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-14 14:20:10 +0530

    Uc2

---
+++ Day22-UsingReflection_MoodAnalyzer/MoodAnalysisException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MoodAnalyzerApp
+{
+    [Serializable]
+    internal class MoodAnalysisException : Exception
+    {
+        private object nO_SUCH_CLASS;
+        private string v;
+
+        public MoodAnalysisException()
+        {
+        }
+
+        public MoodAnalysisException(string message) : base(message)
+        {
+        }
+
+        public MoodAnalysisException(object nO_SUCH_CLASS, string v)
+        {
+            this.nO_SUCH_CLASS = nO_SUCH_CLASS;
+            this.v = v;
+        }
+
+        public MoodAnalysisException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MoodAnalysisException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public static object ExceptionType { get; internal set; }
+    }
+}
